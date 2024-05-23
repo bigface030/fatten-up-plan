6f6eadcc5d96e47bc4d6235cd10f1f87389c4e73
bigface030 <tp953612@gmail.com>
2024-05-23 14:25:59 +0800

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+const globals = require('globals');
+const pluginJs = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    ignores: ['*.js', 'dist'],
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+];
