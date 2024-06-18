25b7ee129a4835285aa75a63c1aef883571b1d49
bigface030 <tp953612@gmail.com>
2024-06-18 17:06:50 +0800

    modify: node pg config

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,19 @@ import { Transact } from './type';
 
 const { Pool, types } = pg;
 
-const pool = new Pool();
+const config =
+  process.env.NODE_ENV === 'production'
+    ? {
+        connectionString: `${process.env.DATABASE_URL}?sslmode=require`,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        connectionString: process.env.DATABASE_URL,
+      };
+
+const pool = new Pool(config);
 
 types.setTypeParser(types.builtins.NUMERIC, Number);
 types.setTypeParser(types.builtins.DATE, (date) => date);
