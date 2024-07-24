11eaf72cbb00d1cc87af4db896905b40cbbf3586
bigface030 <tp953612@gmail.com>
2024-07-24 18:19:20 +0800

    chore: import path alias to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  roots: [compilerOptions.rootDir],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
